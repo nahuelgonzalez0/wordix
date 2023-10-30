770d1d11ec88f960b8b185c6007845f2d5d8ea55
SantiagoPoo <santiago.poo@est.fai.uncoma.edu.ar>
2023-10-29 21:23:11 -0300

    Revisión línea 416

--- wordix.php
+++ wordix.php
@@ -413,7 +413,7 @@ function jugarWordix($palabraWordix, $nombreUsuario){
 
     if ($ganoElIntento) {
         $nroIntento--;
-        $puntaje = obtenerPuntajeWordix();
+        $puntaje = obtenerPuntajeWordix($palabraWordix);
         echo "Adivinó la palabra Wordix en el intento " . $nroIntento . "!: " . $palabraIntento . " Obtuvo $puntaje puntos!";
     } else {
         $nroIntento = 0; //reset intento
